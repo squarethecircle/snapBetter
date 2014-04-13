b853264b4fee7bb275745bf6da0efa87db09581a
Sachith Gullapalli <sachith96@gmail.com>
2014-04-13 16:26:52 -0400

    db

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 import os
 
-SQLALCHEMY_DATABASE_URI = 'postgres://localhost:5432/snapbetter'
+SQLALCHEMY_DATABASE_URI = 'postgres://sxmcazelqcfszj:8mV7XaN_wEgna8uuYTBQH5PyY5@ec2-54-204-2-217.compute-1.amazonaws.com:5432/d3d3ka1rcf20ed'
 
 APP_ROOT = os.path.dirname(os.path.abspath(__file__))
 APP_STATIC = os.path.join(APP_ROOT, 'static')
