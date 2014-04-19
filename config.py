a100d5fdffe6f7e25816eca1afc6a5b2c5d5eb7b
Sachith Gullapalli <sachith96@gmail.com>
2014-04-18 22:13:32 -0400

    login-pg

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 import os
 
-SQLALCHEMY_DATABASE_URI = 'postgres://secret_snapta:3stacksQsort@snapbetter.cdlasjxjiy0g.us-east-1.rds.amazonaws.com:5432/snapbetter'
+SQLALCHEMY_DATABASE_URI = 'postgres://localhost:5432/snapbetter'
 
 APP_ROOT = os.path.dirname(os.path.abspath(__file__))
 APP_STATIC = os.path.join(APP_ROOT, 'static')
